cc78b8351c06330af79d1babee0af517b8649f63
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-08-29 21:03:49 +0300

    10. Color Picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-/*
-React hook - special function that allows functional components
-              to use features without writing class components (React v16.8)
-              (useState, useEffect, useContent, useReducer, usecallback and more...)
-
-useState() - A React hook that allows the creation of a stateful variable
-              AND a setter function to update its value in the virtual DOM
-*/
-
-import MyComponent from "./MyComponent.jsx";
-import Counter from "./Counter.jsx";
 import ColorPicker from "./ColorPicker.jsx";
+
 function App() {
     return(
     <> 
